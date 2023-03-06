5392377a049e37d3ba393ef857a6ed6798b08062
maikeliani <maikel.iani31@gmail.com>
2023-03-05 22:01:49 -0300

    Add project files.

---
+++ Soma_Par_Impar_prof/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 0, somaPar = 0, somaImpar = 0;
+
+        do
+        {
+            Console.WriteLine( " Informe um numero inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            if( ( num < 1000) && (num > 0))
+            {
+                if (num % 2 == 0)
+                {
+                    somaPar += num; 
+                }
+                else
+                {
+                    somaImpar += num; 
+                }
+            }
+            
+
+        } while (num < 1000);
+        Console.WriteLine(" A soma dos numeros pares é: " + somaPar);
+        Console.WriteLine(" A soma dos numeros impares é: " + somaImpar);
+    }
+}
